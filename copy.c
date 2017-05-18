e357c11884cb844d8377ae2eb9b409ab911f21d5
Vsevolod Strukchinsky <floatdrop@users.noreply.github.com>
2017-05-18 13:33:00 +0500

    Implement reading from stdin (#2)

--- copy.c
+++ copy.c
@@ -1,11 +1,56 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <windows.h>
 
-int wmain(int argc, wchar_t **argv) {
-	DWORD cbSize = (wcslen(argv[1]) + 1) * 2;
+typedef struct node_tag {
+	char buffer[1024];
+	int capacity;
+	struct node_tag *next;
+} node;
+
+int wmain() {
+	_setmode(_fileno(stdin), _O_U8TEXT);
+
+	node *bufferList = malloc(sizeof(node));
+	if (bufferList == NULL) {
+		wprintf(L"Could not allocate memory for reading stdin.\n");
+		return 1;
+	}
+	bufferList->capacity = 0;
+	node *head = bufferList;
+
+	DWORD cbSize = 0;
+	DWORD count;
+	while ((count = fread(head->buffer, 1, sizeof(head->buffer), stdin)) != 0) {
+		cbSize += count;
+		head->capacity = count;
+		head->next = malloc(sizeof(node));
+		if (head->next == NULL) {
+			wprintf(L"Could not allocate memory for reading stdin.\n");
+			return 1;
+		}
+		head->next->capacity = 0;
+		head = head->next;
+	}
+
+	if (ferror(stdin)) {
+		fwprintf(stderr, L"Reading stdin failed.\r\n");
+		return 1;
+	}
+
 	HANDLE hgMem = GlobalAlloc(GMEM_MOVEABLE, cbSize);
 	LPBYTE lpData = GlobalLock(hgMem);
-	CopyMemory(lpData, argv[1], cbSize);
+
+	cbSize = 0;
+	head = bufferList;
+	while (head->capacity != 0) {
+		CopyMemory(lpData + cbSize, head->buffer, head->capacity);
+		cbSize += head->capacity;
+		node *prev = head;
+		head = head->next;
+		free(prev);
+	}
+
 	GlobalUnlock(hgMem);
 	OpenClipboard(NULL);
 	EmptyClipboard();
