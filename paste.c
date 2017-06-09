3120aa1962b8d021f2c911598f9350487b4698a4
Vsevolod Strukchinsky <floatdrop@users.noreply.github.com>
2017-06-10 02:42:07 +0500

    Fix off-by-one error with line ending character (#7)

--- paste.c
+++ paste.c
@@ -5,10 +5,14 @@
 int wmain() {
 	OpenClipboard(NULL);
 
-	HANDLE clip = GetClipboardData(CF_UNICODETEXT);
-	if (clip) {
-		_setmode(_fileno(stdout), _O_U8TEXT);
-		wprintf(L"%s\n", clip);
+	HANDLE hData = GetClipboardData(CF_UNICODETEXT);
+	if (hData) {
+		wchar_t *pText = (wchar_t *) GlobalLock(hData);
+		if (pText) {
+			_setmode(_fileno(stdout), _O_U8TEXT);
+			fputws(pText, stdout);
+			GlobalUnlock(hData);
+		}
 	}
 
 	CloseClipboard();
