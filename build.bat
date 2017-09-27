5eb1d65dfee35004498ae4d9d61591c6a8848e86
Vsevolod Strukchinsky <floatdrop@gmail.com>
2017-09-27 11:24:52 +0500

    Build Win32 binaries

--- build.bat
+++ build.bat
@@ -1,9 +1,25 @@
 @echo off
 
+set PATH=C:\MinGW\bin;%PATH%
+
+mkdir Win32
+
 windres --codepage=65001 copy-meta.rc copy-meta.o
-gcc copy.c copy-meta.o -municode -O2 -s -o copy.exe -std=c99
+gcc copy.c copy-meta.o -municode -O2 -s -o Win32/copy.exe -std=c99
 del copy-meta.o
 
 windres --codepage=65001 paste-meta.rc paste-meta.o
-gcc paste.c paste-meta.o -municode -O2 -s -o paste.exe -std=c99
+gcc paste.c paste-meta.o -municode -O2 -s -o Win32/paste.exe -std=c99
+del paste-meta.o
+
+set PATH=C:\mingw-w64\x86_64-6.3.0-posix-seh-rt_v5-rev1\mingw64\bin;%PATH%
+
+mkdir Win64
+
+windres --codepage=65001 copy-meta.rc copy-meta.o
+gcc copy.c copy-meta.o -municode -O2 -s -o Win64/copy.exe -std=c99
+del copy-meta.o
+
+windres --codepage=65001 paste-meta.rc paste-meta.o
+gcc paste.c paste-meta.o -municode -O2 -s -o Win64/paste.exe -std=c99
 del paste-meta.o
