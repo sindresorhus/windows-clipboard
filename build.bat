91273c5044a5aeba7a849ddcfeb9791a291207c9
Vsevolod Strukchinsky <floatdrop@gmail.com>
2017-09-27 11:30:15 +0500

    Fix build.bat
    Fix call to windres

--- build.bat
+++ build.bat
@@ -1,16 +1,15 @@
 @echo off
 
-windres --codepage=65001 copy-meta.rc copy-meta.o
-windres --codepage=65001 paste-meta.rc paste-meta.o
-
 set PATH=C:\MinGW\bin;%PATH%
 
 echo "Building Win32 binaries"
 mkdir Win32
 
+windres --codepage=65001 copy-meta.rc copy-meta.o
 gcc copy.c copy-meta.o -mwin32 -O2 -s -o Win32/copy.exe -std=c99
 del copy-meta.o
 
+windres --codepage=65001 paste-meta.rc paste-meta.o
 gcc paste.c paste-meta.o -mwin32 -O2 -s -o Win32/paste.exe -std=c99
 del paste-meta.o
 
@@ -19,8 +18,10 @@ set PATH=C:\mingw-w64\x86_64-6.3.0-posix-seh-rt_v5-rev1\mingw64\bin;%PATH%
 echo "Building Win64 binaries"
 mkdir Win64
 
+windres --codepage=65001 copy-meta.rc copy-meta.o
 gcc copy.c copy-meta.o -municode -O2 -s -o Win64/copy.exe -std=c99
 del copy-meta.o
 
+windres --codepage=65001 paste-meta.rc paste-meta.o
 gcc paste.c paste-meta.o -municode -O2 -s -o Win64/paste.exe -std=c99
 del paste-meta.o
