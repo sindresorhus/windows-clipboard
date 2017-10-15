5a3c6225cd6d10a427fce1ce0c7e3f03b38effca
Vsevolod Strukchinsky <floatdrop@gmail.com>
2017-10-15 10:49:41 +0500

    Use MinGW

--- build.bat
+++ build.bat
@@ -1,17 +1,19 @@
 @echo off
 
-set PATH=C:\mingw-w64\x86_64-6.3.0-posix-seh-rt_v5-rev1\mingw64\bin;%PATH%
+set PATH=C:\MinGW\bin;%PATH%
 
 echo "Building Win32 binaries"
 mkdir Win32
 
 windres --codepage=65001 copy-meta.rc copy-meta.o
-gcc copy.c copy-meta.o -m32 -municode -O2 -s -o Win32/copy.exe -std=c99
+gcc copy.c copy-meta.o -O2 -s -o Win32/copy.exe -std=c99
 del copy-meta.o
 
 windres --codepage=65001 paste-meta.rc paste-meta.o
-gcc paste.c paste-meta.o -m32 -municode -O2 -s -o Win32/paste.exe -std=c99
+gcc paste.c paste-meta.o -O2 -s -o Win32/paste.exe -std=c99
 del paste-meta.o
+
+set PATH=C:\mingw-w64\x86_64-6.3.0-posix-seh-rt_v5-rev1\mingw64\bin;%PATH%
 
 echo "Building Win64 binaries"
 mkdir Win64
